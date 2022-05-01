a04544386baa3bc26ea8fdb57f63708787ac3803
Barry <barry@kokorozashis-MacBook-Pro.local>
2022-05-01 15:22:27 +0800

    new feature

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,49 @@
 <template>
 
   <el-container>
-    <el-header>Header</el-header>
+    <el-header>
+      <div class="wordHead">5048 - Group 19 Visualisation</div>
+    </el-header>
     <el-container>
-      <el-aside width="300px">
+      <el-aside>
         <div>
-          <el-row class="tac">
-            <el-col :span="12">
-              <el-menu
-                default-active="2"
-                class="el-menu-vertical-demo"
-                background-color="#545c64"
-                text-color="#fff"
-                active-text-color="#ffd04b">
-                <el-menu-item index="1" @click="changeView('Group')">
-                  <i class="el-icon-menu"></i>
-                  <span slot="title">Group</span>
-                </el-menu-item>
-                <el-menu-item index="2" @click="changeView('Influence')">
-                  <i class="el-icon-menu"></i>
-                  <span slot="title">Influence</span>
-                </el-menu-item>
-                <el-menu-item index="3" @click="changeView('Correlation')">
-                  <i class="el-icon-document"></i>
-                  <span slot="title">Correlation</span>
-                </el-menu-item>
-                <el-menu-item index="4" @click="changeView('Continent')">
-                  <i class="el-icon-setting"></i>
-                  <span slot="title">Continent</span>
-                </el-menu-item>
-                <el-menu-item index="5" @click="changeView('Deviation')">
-                  <i class="el-icon-setting"></i>
-                  <span slot="title">Deviation</span>
-                </el-menu-item>
-                <el-menu-item index="6" @click="changeView('Global')">
-                  <i class="el-icon-setting"></i>
-                  <span slot="title">Global</span>
-                </el-menu-item>
-              </el-menu>
-            </el-col>
-          </el-row>
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            background-color="#5CDB9F"
+            text-color="#fff"
+            active-text-color="green">
+            <el-menu-item index="1" @click="changeView('Group')">
+              <i class="el-icon-menu"></i>
+              <span slot="title">Group</span>
+            </el-menu-item>
+            <el-menu-item index="2" @click="changeView('Influence')">
+              <i class="el-icon-menu"></i>
+              <span slot="title">Influence</span>
+            </el-menu-item>
+            <el-menu-item index="3" @click="changeView('Correlation')">
+              <i class="el-icon-document"></i>
+              <span slot="title">Correlation</span>
+            </el-menu-item>
+            <el-menu-item index="4" @click="changeView('Continent')">
+              <i class="el-icon-setting"></i>
+              <span slot="title">Continent</span>
+            </el-menu-item>
+            <el-menu-item index="5" @click="changeView('Deviation')">
+              <i class="el-icon-setting"></i>
+              <span slot="title">Deviation</span>
+            </el-menu-item>
+            <el-menu-item index="6" @click="changeView('Global')">
+              <i class="el-icon-setting"></i>
+              <span slot="title">Global</span>
+            </el-menu-item>
+          </el-menu>
+        </div>
+        <div class="Intro">
+          <el-card class="box-card" style="margin-top: 10px;height: 380px">
+            <h1>Intro</h1>
+            <p>Hello World</p>
+          </el-card>
         </div>
       </el-aside>
       <el-main>
@@ -75,7 +79,7 @@
 export default {
   data() {
     return {
-      currentView: ''
+      currentView: '',
     }
   },
   methods: {
@@ -92,15 +96,41 @@ export default {
 </script>
 
 <style>
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  padding: 18px 0;
+}
+
+.box-card {
+  width: 295px;
+}
+
+
 .wrap {
-  width: 1440px;
-  height: 1580px;
+  width: 1150px;
+  height: 690px;
   overflow: hidden;
 }
 
+.el-header {
+  background-color: #5CDB9F;
+}
+
+.wordHead {
+  color: black;
+}
+
+.el-icon-menu {
+  color: #42b983;
+}
+
 iframe {
-  width: 150% !important;
-  height: 100% !important;
+  width: 165% !important;
+  height: 200% !important;
   -webkit-transform: scale(0.6);
   transform: scale(0.6);
   -webkit-transform-origin: 0 0;
